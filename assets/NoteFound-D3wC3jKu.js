2ae2e01b5de51199b7a257fde43340dc053649d0
AgentSingle <AgentSingle@users.noreply.github.com>
2024-04-17 22:42:24 +0000

    deploy: 2994cc0e383d77f1ede6c4fd8d742f64d6743bfd

--- assets/NoteFound-D3wC3jKu.js
+++
@@ -1,1 +0,0 @@
-import{g as o}from"./index-BjyV1sKA.js";const u={__name:"NoteFound",setup(t){return o(()=>{router.go(-1)}),(e,r)=>" Not Found! "}};export{u as default};
---
+++ assets/NoteFound-DmRk-JCk.js
@@ -0,0 +1,1 @@
+const n={__name:"NoteFound",setup(t){return(e,_)=>" Not Found! "}};export{n as default};
